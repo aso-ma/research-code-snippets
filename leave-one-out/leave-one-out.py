e21dcf5482cdef0db715844d686ea0f1e56970d9
Aso <mafakheri.asoo@gmail.com>
2024-10-25 23:22:44 +0330

    Modifying the evaluation according to the aggregate of all test predictions

--- leave-one-out/leave-one-out.py
+++ leave-one-out/leave-one-out.py
@@ -1,4 +1,4 @@
-from sklearn import datasets
+from sklearn.datasets import make_classification
 from sklearn.model_selection import LeaveOneOut
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
@@ -9,37 +9,33 @@ import warnings
 
 def leave_one_out_for(data, target, classifiers, scoring_metrics):
     loo = LeaveOneOut()
-    results = {clf.__class__.__name__: {metric.__name__: [] for metric in scoring_metrics} for clf in classifiers}
+    results = {clf.__class__.__name__: {metric.__name__: None for metric in scoring_metrics} for clf in classifiers}
 
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
         for clf in classifiers:
             print(clf.__class__.__name__)
+            y_true, y_pred = [], []
             for train_idx, test_idx in tqdm(loo.split(data), total=data.shape[0]):
                 X_train, X_test = data[train_idx], data[test_idx]
                 y_train, y_test = target[train_idx], target[test_idx]
-
                 clf.fit(X_train, y_train)
-                y_pred = clf.predict(X_test)
-                for metric in scoring_metrics:
-                    score = metric(y_test, y_pred)
-                    results[clf.__class__.__name__][metric.__name__].append(score)
+                y_pred.append(clf.predict(X_test)[0])
+                y_true.append(y_test[0])   
 
-    avg_results = {}
-    for clf_name, metrics in results.items():
-        avg_results[clf_name] = {metric: sum(scores)/len(scores) for metric, scores in metrics.items()}
-    return avg_results
+            for metric in scoring_metrics:
+                score = metric(y_true, y_pred)
+                results[clf.__class__.__name__][metric.__name__] = round(score, 5)
+        
+    return results
 
 if __name__ == "__main__":
-    df_iris = datasets.load_iris()
-    X = df_iris.data  
-    y = df_iris.target 
+    X, y = make_classification(n_samples=100, n_features=5, n_classes=2, random_state=42)
 
     classifiers = [RandomForestClassifier(), LogisticRegression(max_iter=250)]
     metrics = [accuracy_score, f1_score, recall_score]
 
     result = leave_one_out_for(X, y, classifiers, metrics)
-
     print(result)
 
 
